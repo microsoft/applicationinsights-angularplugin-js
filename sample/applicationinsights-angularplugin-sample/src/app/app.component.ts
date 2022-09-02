1354448e53f289df5c6659934ed0bc996158a5d0
Karlie Li <aiweili@microsoft.com>
2022-09-02 15:30:54 -0700

    add sample

---
+++ sample/applicationinsights-angularplugin-sample/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { ActivatedRouteSnapshot, ResolveEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { ApplicationInsightsService } from './telemetry.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { 
+  title = 'insights';
+  private appInsights: ApplicationInsightsService
+
+  constructor(private router: Router ) {
+    this.appInsights = new ApplicationInsightsService(router);
+  }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event): event is ResolveEnd => event instanceof ResolveEnd))
+      .subscribe((event) => {
+        const activatedComponent = this.getActivatedComponent(event.state.root);
+        if (activatedComponent) {
+          this.appInsights.trackPageView({name:activatedComponent.name, uri: event.urlAfterRedirects});
+        }
+      });
+  }
+
+  private getActivatedComponent(snapshot: ActivatedRouteSnapshot): any {
+    if (snapshot.firstChild) {
+      return this.getActivatedComponent(snapshot.firstChild);
+    }
+    return snapshot.component;
+  }
+}
---
+++ sample/applicationinsights-angularplugin-sample/src/app/telemetry.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { ResolveEnd, Router } from '@angular/router';
+import { AngularPlugin } from '@microsoft/applicationinsights-angularplugin-js';
+import { ApplicationInsights, ICustomProperties, IDependencyTelemetry, IEventTelemetry, IExceptionTelemetry, IMetricTelemetry, IPageViewTelemetry, ITraceTelemetry } from '@microsoft/applicationinsights-web';
+import { filter } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class ApplicationInsightsService {
+    private angularPlugin = new AngularPlugin();
+    private appInsights = new ApplicationInsights({
+        config: {
+            connectionString: environment.connectionString,
+            extensions: [this.angularPlugin],
+            // auto router tracking, default pageview duration will be set to 0
+            // extensionConfig: {
+            //     [this.angularPlugin.identifier]: {
+            //         router: this.router,
+            //     },
+            // },
+        },
+    });
+
+    constructor(private router: Router) {
+        this.appInsights.loadAppInsights();
+        this.appInsights.setAuthenticatedUserContext("UserContext");
+        this.appInsights.addTelemetryInitializer(envelope => {
+            envelope.tags = envelope.tags || [];
+            envelope.tags["ai.cloud.role"] = "testTag";
+        });
+    }
+
+    // expose tracking methods 
+    trackEvent(event: IEventTelemetry, customProperties?:ICustomProperties) {
+        this.appInsights.trackEvent(event, customProperties);
+    }
+    
+    startTrackEvent(name?: string) {
+        this.appInsights.startTrackEvent(name);
+    }
+    
+    stopTrackEvent(name: string, properties?: { [p: string]: string }, measurements?: { [p: string]: number }): any {
+        this.appInsights.stopTrackEvent(name, properties, measurements);
+    }
+    
+    trackPageView(pageView?:IPageViewTelemetry) {
+        this.appInsights.trackPageView(pageView);
+    }
+    
+    startTrackPage(name?: string) {
+        this.appInsights.startTrackPage(name);
+    }
+    
+    stopTrackPage(name?: string, url?: string, properties?: { [name: string]: string }, measurements?: { [name: string]: number }) {
+        this.appInsights.stopTrackPage(name, url, properties, measurements);
+    }
+    
+    trackMetric(metric:IMetricTelemetry, properties?: ICustomProperties) {
+        this.appInsights.trackMetric(metric, properties);
+    }
+    
+    trackException(exception: IExceptionTelemetry,  properties?: ICustomProperties) {
+        this.appInsights.trackException(exception, properties);
+    }
+    
+    trackTrace(message: ITraceTelemetry, properties?: ICustomProperties) {
+        this.appInsights.trackTrace(message, properties);
+    }
+    
+    trackDependencyData(dependency: IDependencyTelemetry) {
+        this.appInsights.trackDependencyData(dependency);
+    }
+    
+    flush() {
+        this.appInsights.flush();
+    } 
+}
