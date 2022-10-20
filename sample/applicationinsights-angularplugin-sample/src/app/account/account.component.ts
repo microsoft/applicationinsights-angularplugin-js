80530196f4331ffeee1c4507d35649d842cfa1e7
Karlie-777 <79606506+Karlie-777@users.noreply.github.com>
2022-10-20 10:39:40 -0700

    [Task]14057227: Add Angular Plugin Sample (#75)
    * add sample
    
    * remove unused import

---
+++ sample/applicationinsights-angularplugin-sample/src/app/account/account.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent {}
