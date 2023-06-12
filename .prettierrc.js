934e566773b593458e9f5fa0cbaa114d4d0e51c7
arturovt <arthurandrosovich@gmail.com>
2023-06-12 23:38:22 +0300

    Setup Husky and Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    printWidth: 120,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'none',
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: process.platform === 'win32' ? 'auto' : 'lf'
+};
