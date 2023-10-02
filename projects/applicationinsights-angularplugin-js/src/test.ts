50d42ce0502b28acd54236745c45009f152fe0a4
siyuniu-ms <siyuniu@microsoft.com>
2023-10-02 13:47:00 -0700

    update dep

--- projects/applicationinsights-angularplugin-js/src/test.ts
+++ projects/applicationinsights-angularplugin-js/src/test.ts
@@ -4,13 +4,9 @@ import "zone.js";
 import "zone.js/testing";
 import { getTestBed } from "@angular/core/testing";
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting} from "@angular/platform-browser-dynamic/testing";
-
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    <T>(id: string): T;
-    keys(): string[];
-  };
-};
+import "./lib/applicationininsights-angularplugin-mulple-instace.spec";
+import "./lib/applicationinsights-angularplugin-error.service.spec";
+import "./lib/applicationinsights-angularplugin-js.component.spec";
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
@@ -18,8 +14,3 @@ getTestBed().initTestEnvironment(
     platformBrowserDynamicTesting(), {
         teardown: { destroyAfterEach: false }
     });
-
-// Then we find all the tests.
-const context = require.context("./", true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
